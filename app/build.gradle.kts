c64ae0752e642b8611f75c60ca4276a6b043a968
Gensik Rubio <143044695+GensikR@users.noreply.github.com>
2023-11-17 02:55:48 -0600

    Pause Menu Implemented. Need to implement way to pause whole game.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
-    implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34"))
+    //TODO: Uncomment next two lines if you want to generate javadoc. DO NOT FORGET TO COMMENT THEM BACK OR THE APP WILL NOT START
+    //implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
+    //implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/whack_it/game/Game_Activity.java
+++ app/src/main/java/com/example/whack_it/game/Game_Activity.java
@@ -1,16 +1,21 @@
 package com.example.whack_it.game;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.whack_it.Main_Activity;
 import com.example.whack_it.mk_mole.Mole;
 import com.example.whack_it.R;
 
@@ -21,14 +26,23 @@ import java.util.ArrayList;
  */
 public class Game_Activity extends AppCompatActivity
 {
-    //TODO: Sound buttons to mute/unmute game same for vibration
-    //TODO: Maybe Button to pause game/return to menu
     //List to hold the IDs of mole views that will be animated
     public static ArrayList<ImageView> mole_viewsId_list = new ArrayList<>();
     private Game_Instance game_instance;
     private CountDownTimer timer;
     private TextView timer_text;
     private EditText total_points_txt;
+    private View pause_view;
+    private Button pause_btn;
+    private Button mute_btn;
+    private Button vibration_btn;
+    private Button exit_btn;
+    private Button continue_btn;
+    private boolean is_muted = false;
+    private AudioManager audioManager;
+    private TextView pause_text;
+    private Vibrator vibrator;
+    private boolean is_vibrating = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +50,24 @@ public class Game_Activity extends AppCompatActivity
         // Initializes the activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        this.audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        this.vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        //Set Pause Menu Up
+        this.pause_view = findViewById(R.id.pause_view);
+        this.pause_text = findViewById(R.id.pauseTXT);
+        this.pause_btn = findViewById(R.id.pauseBTN);
+        this.mute_btn = findViewById(R.id.muteBTN);
+        this.vibration_btn = findViewById(R.id.vibrationBTN);
+        this.continue_btn = findViewById(R.id.continueBTN);
+        this.exit_btn = findViewById(R.id.exitBTN);
+        //Set Buttons Up
+        set_pause_btn();
+        set_mute_btn();
+        set_vibration_btn();
+        set_continue_btn();
+        set_exit_btn();
 
         // Populates the mole_viewsId_list with mole views from the layout
         populate_mole_views_list();
@@ -57,6 +89,87 @@ public class Game_Activity extends AppCompatActivity
 
         //Set point tracker
         this.total_points_txt = findViewById(R.id.points);
+    }
+
+    private void set_vibration_btn()
+    {   //TODO: Implement Vibration
+        this.vibration_btn.setOnClickListener( v ->
+        {
+            if (this.is_vibrating)
+            {
+                // Turn off vibration
+                this.vibration_btn.setText("Turn Vibration: ON");
+                this.vibrator.cancel();
+                this.is_vibrating = false;
+            }
+            else
+            {
+                // Turn on vibration
+                this.vibration_btn.setText("Turn Vibration: OFF");
+                long[] pattern = {0, 1000, 1000}; // Vibration pattern (0ms delay, vibrate for 1000ms, pause for 1000ms)
+                vibrator.vibrate(pattern, 0);
+                this.is_vibrating = true;
+            }
+        });
+    }
+
+    private void set_mute_btn()
+    {
+        this.mute_btn.setOnClickListener( v ->
+        {//TODO: Implement Audio
+            if (this.is_muted)
+            {
+                // Unmute the game
+                this.mute_btn.setText("Mute");
+                this.audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.USE_DEFAULT_STREAM_TYPE, 0);
+            }
+            else
+            {
+                // Mute the game
+                this.mute_btn.setText("Unmute");
+                this.audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+            }
+            this.is_muted = !this.is_muted;
+        });
+    }
+
+    private void set_exit_btn()
+    {
+        this.exit_btn.setOnClickListener( v ->
+        {   //TODO: Make sure to clear everything from exited game
+            //Changes to game over activity
+            Intent intent = new Intent(Game_Activity.this, Game_Over_Activity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void set_continue_btn()
+    {
+        this.continue_btn.setOnClickListener( v ->
+        {   //TODO: implement way to continue game from pause state
+            //Make Everything Disappear
+            this.pause_view.setVisibility(View.GONE);
+            this.pause_text.setVisibility(View.GONE);
+            this.mute_btn.setVisibility(View.GONE);
+            this.vibration_btn.setVisibility(View.GONE);
+            this.continue_btn.setVisibility(View.GONE);
+            this.exit_btn.setVisibility(View.GONE);
+            this.pause_view.setVisibility(View.GONE);
+        });
+    }
+
+    private void set_pause_btn()
+    {
+        this.pause_btn.setOnClickListener( v ->
+        {   //TODO: Implement way to pause the whole game
+            //Make Everything Visible
+            this.pause_view.setVisibility(View.VISIBLE);
+            this.pause_text.setVisibility(View.VISIBLE);
+            this.mute_btn.setVisibility(View.VISIBLE);
+            this.vibration_btn.setVisibility(View.VISIBLE);
+            this.continue_btn.setVisibility(View.VISIBLE);
+            this.exit_btn.setVisibility(View.VISIBLE);
+        });
     }
 
     /**
