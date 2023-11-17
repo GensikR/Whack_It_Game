1a355fef847ecb882ebe730d156051554ace1424
Gensik Rubio <143044695+GensikR@users.noreply.github.com>
2023-11-16 21:04:20 -0600

    Javadoc generated for better documentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"))
+    implementation(files("C:\\Users\\gensi\\AppData\\Local\\Android\\Sdk\\platforms\\android-34"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
